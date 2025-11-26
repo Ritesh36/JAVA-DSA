d08789002f52bf298efd64ea29bffc4872d63acf
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-11-26 23:58:37 +0530

    feat: add `addLast`, `removeFirst`, and `removeLast` methods, and refactor `addFirst` logic in DoublyLL.

--- DoublyLL.java
+++ DoublyLL.java
@@ -4,7 +4,7 @@ class DoublyLL {
         Node next;
         Node prev;
 
-        Node (int data) {
+        Node(int data) {
             this.data = data;
             this.next = null;
             this.prev = null;
@@ -17,12 +17,8 @@ class DoublyLL {
 
     public void addFirst(int data) {
         Node newNode = new Node(data);
-        if(head == null) {
-            head = newNode;
-            return;
-        }
-
-        if(size == 1) {
+        size++;
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -32,13 +28,68 @@ class DoublyLL {
         head = newNode;
     }
 
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    public int removeFirst() {
+        if(head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if (head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        Node temp = head;
+        for(int i=0; i<size-2; i++) {
+            temp = temp.next;
+        }
+
+        int val = tail.data;
+        temp.next = null;
+        tail.prev = null;
+        tail = temp;
+        return val;
+    }
+
     public void print() {
         Node temp = head;
-        while(temp != null) {
+        while (temp != null) {
             System.out.print(temp.data + " <-> ");
             temp = temp.next;
         }
         System.out.print("null");
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -46,6 +97,10 @@ class DoublyLL {
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
+        dll.addLast(4);
+        dll.print();
+
+        dll.removeLast();
         dll.print();
     }
 }
