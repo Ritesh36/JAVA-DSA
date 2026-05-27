b4ef4f89f26e98e4948acc08c2ab10997678e0d1
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2026-05-27 15:15:17 +0530

    feat: implement knapSack and houseRobber methods with memoization in DP class

--- DP.java
+++ DP.java
@@ -61,15 +61,114 @@ public class DP {
 
         return dp[n];
     }
+
+    //Recursion
+    public static int knapSack(int val[], int wt[], int capacity, int idx) {
+        if (capacity == 0 || idx == 0) {
+            return 0;
+        }
+
+        if (wt[idx-1] <= capacity) { // valid
+            //include
+            int pick = val[idx-1] + knapSack(val, wt, capacity - wt[idx-1], idx-1);
+            //exclude
+            int notPick = 0 + knapSack(val, wt, capacity, idx-1);
+            return Math.max(pick, notPick);
+        } else { // not valid
+            return knapSack(val, wt, capacity, idx-1);
+        }
+    }
+
+    //Memoization
+    public static int knapSackMemo(int val[], int wt[], int capacity, int idx, int dp[][]) {
+        if (capacity == 0 || idx == 0) {
+            return 0;
+        }
+
+        if (dp[idx][capacity] != -1) {
+            return dp[idx][capacity];
+        }
+
+        if (wt[idx-1] <= capacity) {
+            int pick = val[idx-1] + knapSackMemo(val, wt, capacity - wt[idx-1], idx-1, dp);
+            int notPick = 0 + knapSackMemo(val, wt, capacity, idx-1, dp);
+            dp[idx][capacity] = Math.max(pick, notPick);
+            return dp[idx][capacity];
+        } else {
+            dp[idx][capacity] = knapSackMemo(val, wt, capacity, idx-1, dp);
+            return dp[idx][capacity];
+        }
+    }
+
+    public static int climbStairs(int cost[], int idx, int dp[]) {
+        if (idx == 0 || idx == 1) {
+            return cost[idx];
+        }
+
+        if (idx < 0) {
+            return 0;
+        }
+
+        if (dp[idx] != 0) {
+            return dp[idx];
+        }
+
+        int cost1 = climbStairs(cost, idx - 1, dp);
+        int cost2 = climbStairs(cost, idx - 2, dp);
+        dp[idx] = cost[idx] + Math.min(cost1, cost2);
+        return dp[idx];
+    }
+
+    public static int houseRobber(int nums[], int idx, int dp[]) {
+        if (idx == 0) {
+            return nums[idx];
+        }
+
+        if (idx < 0) {
+            return 0;
+        }
+
+        if (dp[idx] != 0) {
+            return dp[idx];
+        }
+
+        int pick = nums[idx] + houseRobber(nums, idx - 2, dp);
+        int notPick = 0 + houseRobber(nums, idx - 1, dp);
+        dp[idx] = Math.max(pick, notPick);
+        return dp[idx];
+    }
     public static void main(String[] args) {
         // int n = 6;
         // int f[] = new int[n+1];
         // System.out.println(fib(n, f));
         // System.out.println(fibTabulization(n));
 
-        int n = 5;
-        int ways[] = new int[n+1];
-        System.out.println(countWays(n, ways));
-        System.out.println(countWaysTab(n));
+        // int n = 5;
+        // int ways[] = new int[n+1];
+        // System.out.println(countWays(n, ways));
+        // System.out.println(countWaysTab(n));
+
+        // int val[] = {15, 14, 10, 45, 30};
+        // int wt[] = {2, 5, 1, 3, 4};
+        // int capacity = 7;
+        // int idx = val.length;
+        // int dp[][] = new int[idx+1][capacity+1];
+        // for (int i=0; i<dp.length; i++) {
+        //     for (int j=0; j<dp[0].length; j++) {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // System.out.println(knapSackMemo(val, wt, capacity, idx, dp));
+
+        // int cost[] = {10, 15, 20};
+        // int idx = cost.length - 1;
+        // int dp[] = new int[cost.length];
+        // System.out.println(climbStairs(cost, idx, dp));
+
+        int nums[] = {5, 15, 6, 20, 22};
+        int idx = nums.length - 1;
+        int dp[] = new int[nums.length];
+        System.out.println(houseRobber(nums, idx, dp));
+
     }
 }
